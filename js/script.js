7cc2829f4794ac191f2d7b0ad1008c58af16e151
Krystin Villeneuve <krystinv@gmail.com>
2016-12-07 14:19:11 -0700

    Add additional JS functionality

--- js/script.js
+++ js/script.js
@@ -2,16 +2,30 @@ var guess = document.getElementById('user-guess');
 var guessButton = document.getElementById('user-guess-button');
 var inputDisplay = document.getElementById('user-input-display');
 var clearButton = document.getElementById('clear-button');
-var rng = Math.floor(Math.random()*100);
+var resetButton = document.getElementById('reset');
+var rng = Math.floor((Math.random() * 100))%100  + 1;
+
 
 guessButton.addEventListener('click', function () {
   onScreenGuess();
-});
+  var num = parseInt(guess.value);
+  if (num === rng) {
+    console.log('BOOM!');
+  } else if (num > 100 || num < 1){
+    console.log('What do you think you are doing?!');
+  } else if (num < rng) {
+    console.log('That is too low!');
+  } else if (num > rng){
+    console.log('That is too high!')
+}});
 
 clearButton.addEventListener('click', function () {
   clearField();
-
 });
+
+resetButton.addEventListener('click', function(){
+  clearField();
+})
 
 function onScreenGuess () {
   var display = guess.value;
@@ -22,9 +36,6 @@ function onScreenGuess () {
 function clearField (){
   var clear="";
   inputDisplay.innerText = clear;
+  guess.value=clear;
   console.log('BYE');
-}
-
-function getRandom() {
-  return Math.floor(Math.random()*100);
 }
