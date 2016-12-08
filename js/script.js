14d7b760d1e7af6bd2f2f72e0481e443d6b3dfeb
Matt Hayden (Valenceo LTD) <hayden+git@morp.holo.gy>
2016-12-07 20:51:22 -0700

    Improved guessing--Now it tracks if the guesser is drunk

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 
 // These are elements in the DOM
-var guess = document.getElementById('user-guess');
-var guessButton = document.getElementById('user-guess-button');
+var guess        = document.getElementById('user-guess');
+var guessButton  = document.getElementById('user-guess-button');
 var inputDisplay = document.getElementById('user-input-display');
-var response = document.getElementById('guess-response');
-var clearButton = document.getElementById('clear-button');
-var resetButton = document.getElementById('reset');
+var response     = document.getElementById('guess-response');
+var clearButton  = document.getElementById('clear-button');
+var resetButton  = document.getElementById('reset');
+/*
 var userMin = document.getElementById('user-min');
 var userMax = document.getElementById('user-max');
+*/
 
-Math.randBetween = function (min,max,asFloat){
+Math.randBetween = function(min, max, asFloat){
   var r = Math.random()*(max-min)+min;
   // This is the terniary operator:
   return asFloat ? r : Math.round(r);
@@ -19,67 +21,75 @@ Math.randBetween = function (min,max,asFloat){
 // This is an integer
 var randomValue = Math.randBetween(1, 100);
 
-guessButton.addEventListener('click', function () {
-  onScreenGuess();
-  });
+// This is an array
+var alreadyGuessed = Array(100+1);
+alreadyGuessed.fill(0);
+
+guessButton.addEventListener('click', function() {
+  setResponse(parseGuess(parseInt(guess.value) ));
+});
 
 clearButton.addEventListener('click', function () {
   clearField();
   guess.value = "";
-  inputDisplay.value = "";
+  setDisplay("");
   clearButton.disabled = true;
-
 });
 
-resetButton.addEventListener('click', function(){
-  clearField();
-})
+// clearField is a function... when clearField is used, it's not called, like so:
+resetButton.addEventListener('click', clearField);
 
-guess.addEventListener('keyup', function(){
-  if(guess.value === "") {
-    clearButton.disabled = true;
-  } else {
-    clearButton.disabled = false;
-  }
+guess.addEventListener('keyup', function() {
+  clearButton.disabled = (guess.value == "");
 });
 
-// resetButton.addEventListener('click', function(){
-//
-// })
 
-window.onload = function (){
-  console.log(randomValue);
+window.onload = function () {
+  console.log("Spoiler! the random value is "+randomValue);
   clearButton.disabled = true;
   resetButton.disabled = true;
 }
 
-function onScreenGuess () {
-  var display = guess.value;
-  inputDisplay.innerText = display;
-  var num = parseInt(guess.value);
-  if (num === randomValue) {
-    response.innerText = "BOOM!";
-  } else if (num > 100 || num < 1){
+function parseGuess(n) {
+  if (n < 1 || 100 < n) {
     alert("Out of range! Please, pick a number between 1 and 100.");
-  } else if (num < randomValue) {
-    response.innerText = "That's too low!";
-  } else if (randomValue < num){
-    response.innerText="That's too high!"
- }
+    return "Try again";
+  }
+  if (alreadyGuessed[n])
+    return "You already guessed "+n+"! Go home!";
+  setDisplay(n);
+  alreadyGuessed[n] += 1;
+  if (n == randomValue) {
+    return "BOOM!";
+  } else if (Math.abs(n - randomValue) < 3) {
+    return "You're getting warmer!";
+  } else if (n < randomValue) {
+    return "That's too low!";
+  } else if (randomValue < n) {
+    return "That's too high!";
+  }
+  return "How did I get here?";
+}
+
+function setResponse(text) {
+  response.innerText = text;
+}
+
+function setDisplay(n) {
+  inputDisplay.innerText = n;
 }
 
 function clearField (){
-  var clear="";
-  inputDisplay.innerText = clear;
-  guess.value=clear;
+  setDisplay("");
+  guess.value="";
 }
 
+/*
 function getMinimum () {
-  var min = userMin.value;
-  var userNum = parseInt(userMin.value);
-  console.log('hi');
+  return parseInt(userMin.value);
 }
 
 function getMaximum() {
-
+  return parseInt(userMax.value);
 }
+*/
